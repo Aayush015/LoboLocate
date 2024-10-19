e4ae4ade77de07a755465958fec18bfdf0078fa4
Aayush <aayushkafle308@gmail.com>
2024-10-18 20:48:39 -0600

    Updated starter page

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+// screens
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import Welcome from './screens/Welcome';
+
+export default function App() {
+  return <Welcome/>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
